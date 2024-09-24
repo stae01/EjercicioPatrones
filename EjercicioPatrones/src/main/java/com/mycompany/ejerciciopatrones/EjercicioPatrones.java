bb6804b3b199e32cc25a478047a12215b9d61af2
stae01 <166432863+stae01@users.noreply.github.com>
2024-09-23 22:17:27 -0700

    a

---
+++ EjercicioPatrones/src/main/java/com/mycompany/ejerciciopatrones/EjercicioPatrones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciopatrones;
+
+/**
+ *
+ * @author 
+ */
+public class EjercicioPatrones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
